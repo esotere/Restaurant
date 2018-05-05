83910eda82658da7573e2e8b2243449e7a449fa3
IVan <georgievigs@gmail.com>
2018-05-05 13:23:19 -0400

    lolz

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var path = require("path");
 
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 const reservations = [];
@@ -16,46 +18,51 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
 app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Displays all characters
+// Displays all reservations
 app.get("/api/reservations", function (req, res) {
     return res.json(reservations);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:reservations", function (req, res) {
+// Displays a single reservation, or returns false
+app.get("/api/reservations/:reservations", function (req, res) {
     var chosen = req.params.reservations;
 
     console.log(chosen);
 
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
+    for (var i = 0; i < reservations.length; i++) {
+        if (chosen === reservations[i].routeName) {
+            return res.json(reservations[i]);
         }
     }
 
     return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function (req, res) {
+// Create New Reservations - takes in JSON input
+app.post("/api/reserve", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
-    var newcharacter = req.body;
+    var newreservation = req.body;
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    // newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newcharacter);
+    console.log(newreservation);
 
-    characters.push(newcharacter);
+    reservations.push(newreservation);
 
-    res.json(newcharacter);
+    res.json(newreservation);
 });
+
 
 
 
